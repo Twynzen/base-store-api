a3a2de35387386d547902c35e5b51cf8507da2ed
Daniel Armando Castiblanco Fajardo <44913158+Twynzen@users.noreply.github.com>
2022-09-29 23:38:45 -0500

    feat: creando nuevas rutas para el servidor

--- index.js
+++ index.js
@@ -5,7 +5,18 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('hola mi server en express')
+        res.send('hola mi server en express')
+    })
+    //Así se crea una ruta
+app.get('/nueva-ruta', (req, res) => {
+        res.send('hola, soy una nueva ruta')
+    })
+    //Podemos con estas rutas colocar jsons los datos que enviaremos
+app.get('/products', (req, res) => {
+    res.json({
+        name: 'producto1',
+        price: 1000
+    })
 })
 
 app.listen(port, () => {
