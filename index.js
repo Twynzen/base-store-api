1eb04c5422b3d5a8d117facc1d5387431307bc60
Daniel Armando Castiblanco Fajardo <44913158+Twynzen@users.noreply.github.com>
2022-10-01 23:03:21 -0500

    feat: creando endpoints

--- index.js
+++ index.js
@@ -13,9 +13,62 @@ app.get('/nueva-ruta', (req, res) => {
     })
     //Podemos con estas rutas colocar jsons los datos que enviaremos
 app.get('/products', (req, res) => {
+    res.json(
+        [{
+                name: 'producto1',
+                price: 1000
+            },
+            {
+                name: 'producto2',
+                price: 3000
+            }
+        ]
+    )
+})
+app.get('/products/:id', (req, res) => {
+    //los {} funcionan para que de todos los parametros de req.paramas, solo recoja el id
+    const { id } = req.params;
+
     res.json({
+        id,
         name: 'producto1',
         price: 1000
+    });
+})
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+    const { categoryId, productId } = req.params;
+    res.json({
+        categoryId,
+        productId
+    })
+
+})
+app.get('/products/:id', (req, res) => {
+    const { id } = req.params
+    res.json({
+        id,
+        name: 'Videojuegos',
+        price: 4000,
+        available: true
+    })
+})
+
+app.get('/categories/:id', (req, res) => {
+    const { id } = req.params
+    res.json({
+        id,
+        name: 'SillasGamer',
+        products: 22
+    })
+})
+
+app.get('/clients/:id', (req, res) => {
+    const { id } = req.params
+    res.json({
+        id,
+        firstName: 'Daniel',
+        lastName: 'Castiblanco'
     })
 })
 
